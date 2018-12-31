20727171f59fb440cc34eb7a2ee45007e35a2792
Erik Schulz <archlinuxuser@protonmail.com>
2018-12-31 11:08:27 +0100

    fulfilled implementation of regression test

--- test/regressiontest.py
+++ test/regressiontest.py
@@ -35,6 +35,7 @@ as the test modules shall be independent from Dotmanager"""
 # Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
 # Programm erhalten haben. Wenn nicht, siehe <https://www.gnu.org/licenses/>.
 
+import hashlib
 import os
 from abc import abstractmethod
 from subprocess import PIPE
@@ -46,8 +47,12 @@ from typing import Union
 
 TestResult = Dict[str, Union[bool, str]]
 CheckResult = Tuple[bool, str]
-FileList = List[Dict[str, Union[bool, int, str]]]
-DirTree = Dict[str, Dict[str, Union[bool, int, FileList]]]
+FileDescriptor = Dict[str, Union[bool, int, str]]
+LinkDescriptor = FileDescriptor
+FileList = List[FileDescriptor]
+LinkList = List[LinkDescriptor]
+DirDescriptor = Dict[str, Union[bool, int, FileList, LinkList]]
+DirTree = Dict[str, DirDescriptor]
 
 # Example for a DirTree
 # {
@@ -64,12 +69,14 @@ DirTree = Dict[str, Dict[str, Union[bool, int, FileList]]]
 #         "links": [
 #             {
 #                 "name": "name.bla1",
+#                 "target": "~/dotfiles/files/name1",
 #                 "permission": 600,
 #                 "rootuser": True,
 #                 "rootgroup": True
 #             },
 #             {
 #                 "name": "test.link",
+#                 "target": "~/dotfiles/files/test",
 #                 "permission": 777,
 #                 "rootuser": False,
 #                 "rootgroup": False
@@ -82,7 +89,7 @@ DirTree = Dict[str, Dict[str, Union[bool, int, FileList]]]
 #     "test/b": {...},
 #     "test/b/c": {...},
 # }
-# Where the content property for files is optional
+# Where the content property is optional for files
 
 
 class RegressionTest():
@@ -168,58 +175,75 @@ class DirRegressionTest(RegressionTest):
         """Checks if dir_tree matches the actual directory
         tree in the filesystem"""
 
-        owd = os.getcwd()
+        def check_owner(path: str, props: Union[DirDescriptor,
+                                                LinkDescriptor,
+                                                FileDescriptor],
+                        is_link: bool = False) -> None:
+            """For owner permissions we only look up if its a normal
+            user or the root user because we can't create other
+            users just for the sake of these tests"""
+            stat = os.lstat if is_link else os.stat
+            if bool(stat(path).st_uid) == props["rootuser"]:
+                user = "user" if props["rootuser"] else "root"
+                raise ValueError((False, f"{path} is a not owned by {user}"))
+            if bool(stat(path).st_gid) == props["rootgroup"]:
+                group = "group" if props["rootgroup"] else "root"
+                raise ValueError((False, f"{path} is a not owned by {group}"))
+
+        def check_permission(path: str, permission: int) -> None:
+            perm_real = str(oct(os.stat(path).st_mode))[-3:]
+            if perm_real != str(permission):
+                raise ValueError((False, f"{path} has permission {perm_real}"))
+
         for dir_name, dir_props in dir_tree:
             # Directory existance
             if not os.path.isdir(dir_name):
-                return (False, dir_name + " is a not a directory")
+                raise ValueError((False, dir_name + " is a not a directory"))
             # Directory permission
-
+            check_permission(dir_name, dir_props["permission"])
             # Directory owner
-            # For owner permissions we only look up if its a normal
-            # user or the root user because we can't create other
-            # users just for the sake of these tests
-            if bool(os.stat(dir_name).st_uid) == dir_props["rootuser"]:
-                user = "user" if dir_props["rootuser"] else "root"
-                return (False, dir_name + " is a not owned by " + user)
-            if bool(os.stat(dir_name).st_gid) == dir_props["rootgroup"]:
-                group = "group" if dir_props["rootgroup"] else "root"
-                return (False, dir_name + " is a not owned by " + group)
-            os.chdir(dir_name)
+            check_owner(dir_name, dir_props)
             # Files
             for file_props in dir_props["files"]:
                 file_path = os.path.join(dir_name, file_props["name"])
                 # File existance
                 if not os.path.isfile(file_path):
-                    return (False, file_path + " is a not a file")
+                    raise ValueError((False, f"{file_path} is a not a file"))
                 # File permission
+                check_permission(file_path, file_props["permission"])
                 # File owner
-                if bool(os.stat(file_path).st_uid) == file_props["rootuser"]:
-                    user = "user" if file_props["rootuser"] else "root"
-                    return (False, file_path + " is a not owned by " + user)
-                if bool(os.stat(file_path).st_gid) == file_props["rootgroup"]:
-                    group = "group" if file_props["rootgroup"] else "root"
-                    return (False, file_path + " is a not owned by " + group)
+                check_owner(file_path, file_props)
                 # File content
+                md5 = hashlib.md5(open(file_path, "rb").read()).hexdigest()
+                if "content" in file_props and md5 != file_props["content"]:
+                    raise ValueError((False, f"{file_path} has wrong content"))
             # Links
             for link_props in dir_props["links"]:
                 link_path = os.path.join(dir_name, link_props["name"])
                 # Link existance
                 if not os.path.islink(link_path):
-                    return (False, link_path + " is a not a link")
+                    raise ValueError((False, f"{link_path} is a not a link"))
                 # Link permission
+                check_permission(link_path, link_props["permission"])
                 # Link owner
-                if bool(os.lstat(link_path).st_uid) == link_props["rootuser"]:
-                    user = "user" if link_props["rootuser"] else "root"
-                    return (False, link_path + " is a not owned by " + user)
-                if bool(os.lstat(link_path).st_gid) == link_props["rootgroup"]:
-                    group = "group" if link_props["rootgroup"] else "root"
-                    return (False, link_path + " is a not owned by " + group)
-            os.chdir(owd)
-        return (True, "")
+                check_owner(link_path, link_props, True)
+                # Link target
+                target_path = os.path.normpath(os.readlink(link_path))
+                if target_path != os.path.normpath(link_props["target"]):
+                    msg = f"{link_path} should point to {link_props['target']}"
+                    msg += ", but points to {target_path}"
+                    raise ValueError((False, msg))
 
     def pre_check(self) -> CheckResult:
-        return self.dircheck(self.before)
+        try:
+            self.dircheck(self.before)
+        except ValueError as err:
+            return err.args[0]
+        return (True, "")
 
     def post_check(self) -> CheckResult:
-        return self.dircheck(self.after)
+        try:
+            self.dircheck(self.after)
+        except ValueError as err:
+            return err.args[0]
+        return (True, "")
